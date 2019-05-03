15a682400845158cf052aff26492343ffe58b57f
sahasransupati <sahasransu.pati@gmail.com>
2019-05-03 08:29:36 +0530

    Assignment 3

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -9,6 +9,17 @@ var User = require('../models/users');
 
 userRouter.use(bodyParser.json());
 
+userRouter.get('/',authenticate.verifyUser,authenticate.verifyAdmin,(req,res,next) =>
+ {
+  User.find({})
+    .then((users) => {
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'application/json');
+      res.json(users);
+  }, (err) => next(err))
+  .catch((err) => next(err));
+})
+
 userRouter.post('/signup', (req, res, next) => {
   User.register(new User({username: req.body.username}), 
     req.body.password, (err, user) => {
