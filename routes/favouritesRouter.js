f56f88cde14cb695183492f6dea714919a4d549a
sahasransupati <sahasransu.pati@gmail.com>
2019-05-10 18:26:55 +0530

    Integrating client and Server

--- routes/favouritesRouter.js
+++ routes/favouritesRouter.js
@@ -77,11 +77,32 @@ favouritesRouter.route('/')
 favouritesRouter.route('/:dishId')
 .options(cors.corsWithOptions, (req, res) => { res.sendStatus(200); })
 .get(cors.corsWithOptions, authenticate.verifyUser, (req,res,next) => {
-    res.statusCode = 403;
-    res.end('GET operation not supported on /favourites/'+ req.params.dishId);
+    Favorites.findOne({user: req.user._id})
+    .then((favorites) => {
+        if (!favorites) {
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            return res.json({"exists": false, "favorites": favorites});
+        }
+        else {
+            if (favorites.dishes.indexOf(req.params.dishId) < 0) {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                return res.json({"exists": false, "favorites": favorites});
+            }
+            else {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                return res.json({"exists": true, "favorites": favorites});
+            }
+        }
+
+    }, (err) => next(err))
+    .catch((err) => next(err))
 })
+  
 .post(cors.corsWithOptions, authenticate.verifyUser, (req, res, next) => {
-    Favourites.findOne({user: req.user._id})
+    Favourites.findOne({user: req.queryuser._id})
     .then((favourite) => {
         if (favourite) {            
             if (favourite.dishes.indexOf(req.params.dishId) === -1) {
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -11,6 +11,8 @@ var User = require('../models/users');
 
 userRouter.use(bodyParser.json());
 
+userRouter.options('*', cors.corsWithOptions, (req, res) => { res.sendStatus(200); } )
+
 userRouter.get('/facebook/token', passport.authenticate('facebook-token'), (req, res) => {
   if (req.user) {
     var token = authenticate.getToken({_id: req.user._id});
@@ -61,6 +63,33 @@ userRouter.post('/signup', cors.corsWithOptions,(req, res, next) => {
   });
 });
 
+userRouter.post('/login', cors.corsWithOptions, (req, res, next) => {
+
+  passport.authenticate('local', (err, user, info) => {
+    if (err)
+      return next(err);
+
+    if (!user) {
+      res.statusCode = 401;
+      res.setHeader('Content-Type', 'application/json');
+      res.json({success: false, status: 'Login Unsuccessful!', err: info});
+    }
+    req.logIn(user, (err) => {
+      if (err) {
+        res.statusCode = 401;
+        res.setHeader('Content-Type', 'application/json');
+        res.json({success: false, status: 'Login Unsuccessful!', err: 'Could not log in user!'});          
+      }
+
+      var token = authenticate.getToken({_id: req.user._id});
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'application/json');
+      res.json({success: true, status: 'Login Successful!', token: token});
+    }); 
+  }) (req, res, next);
+});
+
+/*
 userRouter.post('/login', cors.corsWithOptions,passport.authenticate('local'), (req, res) => {
 
   var token = authenticate.getToken({_id: req.user._id});
@@ -68,7 +97,7 @@ userRouter.post('/login', cors.corsWithOptions,passport.authenticate('local'), (
   res.setHeader('Content-Type', 'application/json');
   res.json({success: true, token: token, status: 'You are successfully logged in!'});
 });
-
+*/
 
 userRouter.get('/logout', cors.corsWithOptions,(req, res) => {
   if (req.session) {
@@ -83,5 +112,23 @@ userRouter.get('/logout', cors.corsWithOptions,(req, res) => {
   }
 });
 
+userRouter.get('/checkJWTtoken', cors.corsWithOptions, (req, res) => {
+  passport.authenticate('jwt', {session: false}, (err, user, info) => {
+    if (err)
+      return next(err);
+    
+    if (!user) {
+      res.statusCode = 401;
+      res.setHeader('Content-Type', 'application/json');
+      return res.json({status: 'JWT invalid!', success: false, err: info});
+    }
+    else {
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'application/json');
+      return res.json({status: 'JWT valid!', success: true, user: user});
+
+    }
+  }) (req, res);
+});
 
 module.exports = userRouter;
